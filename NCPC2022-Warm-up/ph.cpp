b0a23b913bee068d7c38131f5650094fc0eb5e85
AKAAka1450 <58277892+AKAAka1450@users.noreply.github.com>
2022-09-20 19:57:16 +0800

    ph AC

--- NCPC2022-Warm-up/ph.cpp
+++ NCPC2022-Warm-up/ph.cpp
@@ -1,67 +1,55 @@
 #include <iostream>
 #include <vector>
-#include <climits>
 #include <queue>
+#include <climits>
 
 using namespace std;
 
-void BFS(int i, int j, vector<vector<int> > &distances) {
-	queue<pair<int, int> > que;
-	que.push(make_pair(i, j));
-	int count = 0;
+void fire_BFS(queue<pair<int, int> >& que, vector<vector<int> >& fire_dst) {
+	vector<vector<bool> > visited(fire_dst.size(), vector<bool>(fire_dst[0].size(), false));
+	int dst = 0;
 	while ( !que.empty() ) {
 		int size = que.size();
 		for ( int _=0; _<size; _++ ) {
 			int cur_i = que.front().first;
 			int cur_j = que.front().second;
 			que.pop();
-			distances[cur_i][cur_j] = count;
-			if ( cur_i < distances.size()-1 && distances[cur_i+1][cur_j] == INT_MAX ) {
-				que.push(make_pair(cur_i+1, cur_j));
-				distances[cur_i+1][cur_j]--;
-			}
-			if ( cur_i > 0 && distances[cur_i-1][cur_j] == INT_MAX ) {
-				que.push(make_pair(cur_i-1, cur_j));
-				distances[cur_i-1][cur_j]--;
-			}
-			if ( cur_j < distances[0].size()-1 && distances[cur_i][cur_j+1] == INT_MAX ) {
-				que.push(make_pair(cur_i, cur_j+1));
-				distances[cur_i][cur_j+1]--;
-			}
-			if ( cur_j > 0 && distances[cur_i][cur_j-1] == INT_MAX ) {
-				que.push(make_pair(cur_i, cur_j-1));
-				distances[cur_i][cur_j-1]--;
-			}
+			if ( cur_i < 0 || cur_j < 0 || cur_i >= fire_dst.size() || cur_j >= fire_dst[0].size() ) continue;
+			if ( visited[cur_i][cur_j] ) continue;
+			visited[cur_i][cur_j] = true;
+			if ( fire_dst[cur_i][cur_j] == -1 ) continue;
+			fire_dst[cur_i][cur_j] = dst;
+			que.push(make_pair(cur_i-1, cur_j));
+			que.push(make_pair(cur_i+1, cur_j));
+			que.push(make_pair(cur_i, cur_j-1));
+			que.push(make_pair(cur_i, cur_j+1));
 		}
-		count++;
+		dst++;
 	}
 	return;
 }
 
-int escape(int i, int j, vector<vector<int> >& fire_dst) {
-	queue<pair<int, int> > que;
-	que.push(make_pair(i, j));
+int joe_BFS(int s_i, int s_j, vector<vector<int> >& fire_dst) {
 	vector<vector<bool> > visited(fire_dst.size(), vector<bool>(fire_dst[0].size(), false));
-	int time = 0;
+	queue<pair<int, int> > que;
+	que.push(make_pair(s_i, s_j));
+	int dst = 0;
 	while ( !que.empty() ) {
 		int size = que.size();
 		for ( int _=0; _<size; _++ ) {
 			int cur_i = que.front().first;
 			int cur_j = que.front().second;
 			que.pop();
-			if ( fire_dst[cur_i][cur_j] == -1 ) continue;
-			if ( time >= fire_dst[cur_i][cur_j] ) continue;
-			if ( cur_i == 0 || cur_i == fire_dst.size()-1 || cur_j == 0 || cur_j == fire_dst[0].size()-1 ) return time+1;
-			if ( !visited[cur_i-1][cur_j] ) que.push(make_pair(cur_i-1, cur_j));
-			visited[cur_i-1][cur_j] = true;
-			if ( !visited[cur_i+1][cur_j] ) que.push(make_pair(cur_i+1, cur_j));
-			visited[cur_i+1][cur_j] = true;
-			if ( !visited[cur_i][cur_j-1] ) que.push(make_pair(cur_i, cur_j-1));
-			visited[cur_i][cur_j-1] = true;
-			if ( !visited[cur_i][cur_j+1] ) que.push(make_pair(cur_i, cur_j+1));
-			visited[cur_i][cur_j+1] = true;
+			if ( cur_i < 0 || cur_j < 0 || cur_i >= fire_dst.size() || cur_j >= fire_dst[0].size() ) return dst;
+			if ( visited[cur_i][cur_j] ) continue;
+			visited[cur_i][cur_j] = true;
+			if ( fire_dst[cur_i][cur_j] <= dst ) continue;
+			que.push(make_pair(cur_i-1, cur_j));
+			que.push(make_pair(cur_i+1, cur_j));
+			que.push(make_pair(cur_i, cur_j-1));
+			que.push(make_pair(cur_i, cur_j+1));
 		}
-		time++;
+		dst++;
 	}
 	return -1;
 }
@@ -69,41 +57,36 @@ int escape(int i, int j, vector<vector<int> >& fire_dst) {
 int main () {
 	int T;
 	cin >> T;
-	for ( int c=0; c<T; c++ ) {
-		int R,C;
+	for ( int c=1; c<=T; c++ ) {
+		int R, C;
 		cin >> R >> C;
 		vector<vector<char> > maze(R, vector<char>(C));
-		vector<vector<int> > distances(R, vector<int>(C));
-		int fire_i, fire_j;
+		queue<pair<int,int> > fire_que;
 		int joe_i, joe_j;
+		vector<vector<int> > fire_dst(R, vector<int>(C));
 		for ( int i=0; i<R; i++ ) {
 			for ( int j=0; j<C; j++ ) {
 				cin >> maze[i][j];
-				distances[i][j] = maze[i][j] == '#'? -1: INT_MAX;
+				if ( maze[i][j] == '#' ) {
+					fire_dst[i][j] = -1;
+				} else {
+					fire_dst[i][j] = INT_MAX;
+				}
 				if ( maze[i][j] == 'F' ) {
-					fire_i = i;
-					fire_j = j;
+					fire_que.push(make_pair(i, j));
 				} else if ( maze[i][j] == 'J' ) {
 					joe_i = i;
 					joe_j = j;
 				}
 			}
 		}
-		// count distance from fire
-		BFS(fire_i, fire_j, distances);
-		/*
-		for ( int i=0; i<R; i++ ) {
-			for ( int j=0; j< C; j++ ) {
-				cout << distances[i][j] << ' ';
-			}
-			cout << '\n';
-		}*/
-		int res = escape(joe_i, joe_j, distances);
+		fire_BFS(fire_que, fire_dst);
+		int res = joe_BFS(joe_i, joe_j, fire_dst);
 		if ( res == -1 ) {
 			cout << "IMPOSSIBLE\n";
 		} else {
 			cout << res << '\n';
 		}
 	}
-	return 0;
+	return  0;
 }
